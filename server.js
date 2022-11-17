9b7e663c099714a0fa29e2b11840a08581756bd8
InekMarcinek <m.inekblq@o2.pl>
2022-11-17 17:45:19 +0100

    a

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-const bodyParser = require('body-parser')
+const express = require("express");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || "8000";
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({
-    extended: false
-}))
-app.use(flash())
-app.use(session({
+app.set("view engine", "ejs");
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(flash());
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 app.use(bodyParser.text());
-app.use(methodOverride('_method'))
-app.use(express.static("public"))
-app.use(express.json({
-    type: ['application/json', 'text/plain']
-}))
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
 
 app.listen(port, () => {
-    console.log(`Strona dostępna na http://localhost:${port}`);
+  console.log(`Strona dostępna na http://localhost:${port}`);
 });
 
 exports.app = app;
-require('./modules/pages');
+require("./modules/pages");
