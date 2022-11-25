0eed37e76189c74cc2bbf9dcdc8b11f9c954321a
qamarq <kamilm@you2.pl>
2022-11-25 17:08:03 +0100

    dash

--- modules/pages.js
+++ modules/pages.js
@@ -1,5 +1,6 @@
-const {app} = require('../server.js');
+const {app} = require('../server');
 const {checkAuthenticated, checkNotAuthenticated} = require('./functions');
+const {pagePortfolioContent} = require('./portfolio-txt');
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -7,6 +8,15 @@ app.get('/', (req, res) => {
 
 app.get('/dashboard', checkAuthenticated, (req, res) => {
     res.render('dashboard.ejs')
+})
+
+app.get('/portfolio/:page', (req, res) => {
+    const { page } = req.params;
+    const portfolioItem = pagePortfolioContent.get(page)
+    res.render('portfolio.ejs', {
+        title: portfolioItem !== undefined ? portfolioItem.title : "Undefined title",
+        desc: portfolioItem !== undefined ? portfolioItem.desc : "Undefined desc"
+    })
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
---
+++ modules/portfolio-txt.js
@@ -0,0 +1,7 @@
+const pagePortfolioContent = new Map([
+    ["TorunTechnologies", { title: "TORUN Technologies", desc: "opis1" }],
+    ["TorunTechnologiesPanel", { title: "TORUN Technologies Panel", desc: "opis2" }],
+    ["CuprumTorun", { title: "Cuprum Toruń", desc: "opis3" }],
+    ["E-DziennikEducationX", { title: "E-Dziennik EducationX", desc: "opis4" }],
+])
+module.exports.pagePortfolioContent = pagePortfolioContent;
