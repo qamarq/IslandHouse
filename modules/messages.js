43c89d92e26d5f8bd35b08e92f5bc4eb69e7f9c3
qamarq <kamilm@you2.pl>
2022-11-27 10:32:06 +0100

    fix

--- modules/messages.js
+++ modules/messages.js
@@ -6,6 +6,7 @@ const mysql = require('mysql2');
 var {connection} = require('./mysql');
 
 const { Server } = require("socket.io");
+const { clearCache } = require('ejs');
 const io = new Server(server);
 
 const users = {};
@@ -21,32 +22,52 @@ io.on('connection', (socket) => {
     });
     socket.on('typing', (toEmail) => {
         if (users[toEmail] !== undefined) {
-            socket.to(users[toEmail].id).emit("typing", users[toEmail].email);
+            try {
+                socket.to(users[toEmail].id).emit("typing", users[toEmail].email);
+            } catch(e) {
+
+            }
         }
     });
-    socket.on('chat message', (msg) => {
-        const targetUser = users[msg.toEmail];
-        if (targetUser === undefined) {
-            console.log('nie znaleziono');
-            return;
+    socket.on('chat_message', (msg) => {
+        try {
+            const targetUser = users[msg.toEmail];
+            let insertQuery = 'INSERT INTO `messages` (`toEmail`, `toName`, `fromEmail`, `fromName`, `timestamp`, `message`) VALUES (?, ?, ?, ?, ?, ?)';
+            let query = mysql.format(insertQuery, [
+                targetUser.email,
+                targetUser.name,
+                socket.email,
+                socket.name,
+                new Date().getTime(),
+                msg.msg,
+            ]);
+            connection.query(query, (err, response) => {
+                // connection.query(`UPDATE users SET in_progress = 1, attempted = attempted + 1 WHERE email = '${req.user.email}'`);
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                socket.to(users[msg.toEmail].id).emit('chat_message', {fromEmail: socket.email, fromName: socket.name, toEmail: targetUser.email, toName: targetUser.name, message: msg.msg});
+                socket.emit('chat_message', {fromEmail: socket.email, fromName: socket.name, toEmail: targetUser.email, toName: targetUser.name, message: msg.msg});
+            });
+        } catch(e) {
+            let insertQuery = 'INSERT INTO `messages` (`toEmail`, `toName`, `fromEmail`, `fromName`, `timestamp`, `message`) VALUES (?, ?, ?, ?, ?, ?)';
+            let query = mysql.format(insertQuery, [
+                msg.toEmail,
+                msg.toName,
+                socket.email,
+                socket.name,
+                new Date().getTime(),
+                msg.msg,
+            ]);
+            connection.query(query, (err, response) => {
+                // connection.query(`UPDATE users SET in_progress = 1, attempted = attempted + 1 WHERE email = '${req.user.email}'`);
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                socket.emit('chat_message', {fromEmail: socket.email, fromName: socket.name, toEmail: msg.toEmail, toName: msg.toName, message: msg.msg});
+            });
         }
-        let insertQuery = 'INSERT INTO `messages` (`toEmail`, `toName`, `fromEmail`, `fromName`, `timestamp`, `message`) VALUES (?, ?, ?, ?, ?, ?)';
-        let query = mysql.format(insertQuery, [
-            targetUser.email,
-            targetUser.name,
-            socket.email,
-            socket.name,
-            new Date().getTime(),
-            msg.msg,
-        ]);
-        connection.query(query, (err, response) => {
-            // connection.query(`UPDATE users SET in_progress = 1, attempted = attempted + 1 WHERE email = '${req.user.email}'`);
-            if (err) {
-                console.log(err)
-                return;
-            }
-            socket.to(users[msg.toEmail].id).emit('chat message', {fromEmail: socket.email, fromName: socket.name, toEmail: targetUser.email, toName: targetUser.name, message: msg.msg});
-            socket.emit('chat message', {fromEmail: socket.email, fromName: socket.name, toEmail: targetUser.email, toName: targetUser.name, message: msg.msg});
-        });
     });
 });
--- modules/pages.js
+++ modules/pages.js
@@ -16,13 +16,17 @@ app.get('/dashboard', checkAuthenticated, (req, res) => {
 app.get('/portfolio/:page', (req, res) => {
     const { page } = req.params;
     const portfolioItem = pagePortfolioContent.get(page)
-    res.render('portfolio.ejs', {
-        title: portfolioItem !== undefined ? portfolioItem.title : "Undefined title",
-        subtitle: portfolioItem !== undefined ? portfolioItem.subtitle : "Undefined subtitle",
-        content: portfolioItem !== undefined ? portfolioItem.content : "Undefined content",
-        image: portfolioItem !== undefined ? portfolioItem.image : "Undefined image",
-        link: portfolioItem !== undefined ? portfolioItem.link : "Undefined link",
-    })
+    if (portfolioItem !== undefined) {
+        res.render('portfolio.ejs', {
+            title: portfolioItem.title,
+            subtitle: portfolioItem.subtitle,
+            content: portfolioItem.content,
+            image: portfolioItem.image,
+            link: portfolioItem.link,
+        })
+    } else {
+        res.redirect('/');
+    }
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -33,7 +37,9 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
 })
 
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/')
-})
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
